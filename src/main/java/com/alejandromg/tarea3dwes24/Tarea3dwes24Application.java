818e88ea80f6d8e20d32fd7547a2727114430890
alejandromg73 <alejandromg73@educastur.es>
2024-11-20 16:41:31 +0100

    Commit inicial

---
+++ src/main/java/com/alejandromg/tarea3dwes24/Tarea3dwes24Application.java
@@ -0,0 +1,13 @@
+package com.alejandromg.tarea3dwes24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3dwes24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3dwes24Application.class, args);
+	}
+
+}
