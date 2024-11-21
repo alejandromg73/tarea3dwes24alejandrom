32d7bb8cf93e6ee72f2d5190143277ccb93f4dd1
Alejandro <alejandromg73@educastur.es>
2024-11-21 14:11:34 +0100

    Esqueleto del proyecto creado

---
+++ src/main/java/com/alejandromg/tarea3dwes24/Principal.java
@@ -0,0 +1,20 @@
+package com.alejandromg.tarea3dwes24;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+	
+	//   @Autowired
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("INI");
+		
+		
+		System.out.println("----------------------");
+		
+		
+		System.out.println("FIN");
+	}
+
+}
